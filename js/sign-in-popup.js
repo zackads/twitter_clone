342085c039c0c0abdab2818f0a1853a819a94bc4
deanssmart <deanssmart@gmail.com>
2020-08-02 21:59:28 +0100

    Sign in pop up close when clicking any of body

--- js/sign-in-popup.js
+++ js/sign-in-popup.js
@@ -1,32 +1,33 @@
 ((d, w) => {
     let popup = d.getElementById("sign-in-pop");
     let signIn = d.getElementById("sign-in-button");
+    let body = d.getElementById("body");
 
-    signIn.addEventListener("click", () => {
-          if (popup.classList.contains("box-active")) {
-            // hide
-            popup.classList.remove("box-active");
-            popup.classList.add("box-transition");
-            popup.classList.add("box-hidden");
-          } else {
-            // show
-            popup.classList.add("box-visible");
-            popup.clientWidth;
-            console.log(popup.clientWidth);
-            popup.classList.add("box-transition");
-            popup.classList.add("box-active");
-          }
-        },
-        false
-      );
-    
-      popup.addEventListener("transitionend", () => {
-          popup.classList.remove("box-transition");
-          popup.classList.remove("box-visible");
-          popup.classList.remove("box-hidden");
-        },
-        false
-      );
+    body.addEventListener("click", e => {
+      let clicked = e.target;
+      if(clicked.matches("*:not(#sign-in-pop):not(.sign_in)")){
+        console.log(clicked);
+        if (popup.classList.contains("box-active")) {
+          // hide
+          popup.classList.remove("box-active");
+          popup.classList.add("box-transition");
+          popup.classList.add("box-hidden");
+        } else if(clicked.matches("#sign-in-button, #sign-in-button *")) {
+          // show
+          popup.classList.add("box-visible");
+          popup.clientWidth;
+          popup.classList.add("box-transition");
+          popup.classList.add("box-active");
+        }
+      }
+    });
+      
+
+  popup.addEventListener("transitionend", () => {
+      popup.classList.remove("box-transition");
+      popup.classList.remove("box-visible");
+      popup.classList.remove("box-hidden");
+    });
     
 })(document, window);
 
